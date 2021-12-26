8a13ed0009661124599612340abdb87ddfc91daa
AkshitaSagar <94096782+AkshitaSagar@users.noreply.github.com>
2021-12-26 18:35:05 +0530

    Add files via upload

---
+++ gp.js
@@ -0,0 +1,11 @@
+//Write a program to find nth number of geometric progression(GP). 
+//Push your code on Github and share that link here. *
+
+//console.log('hi');
+
+a = prompt('enter number ');
+r=prompt('enter number ');
+n=prompt('enter number series');
+n=parseInt(n);
+output=(a*r* (n-1));
+console.log(output);
